012323420e1425da2fb9b34120449b2d33a3c425
dajung <dajung_ju@hancom.com>
2021-11-14 16:59:32 +0900

    opt test

--- main.c
+++ main.c
@@ -98,7 +98,7 @@ void opt() {
     int i = 0, j = 0, k = 0,index = 0;
     int fault_cnt = 0;
     int frame_list[30] = {-1};
-    int far_list[30] = {100};
+    int far_list[30] = {100,};
 
     memset(frame_list, -1, sizeof(frame_list));
     bool is_fault = true;
